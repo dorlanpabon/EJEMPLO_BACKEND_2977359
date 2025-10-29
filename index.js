eb389bd92525a8bb1e61c5ad8d24d3239eaf913a
Juan Camilo Pabon <jucpabon@nequi.com>
2025-10-28 20:11:08 -0500

    Add package.json with express and mysql2 dependencies

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const mysql = require('mysql2');
+const app = express()
+
+app.use(express.json());
+
+const port = 3000
+//SELECT `id`, `email`, `password` FROM `usuarios` WHERE 1
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  database: 'meli'
+});
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body
+  const [rows] = await pool.promise()
+    .query('SELECT `id`, `email`, `password`' +
+      ' FROM `usuarios` WHERE email = ? AND password = ?',
+      [email, password]);
+  if (rows.length > 0) {
+    res.send('Login Successful')
+  } else {
+    res.send('Invalid Credentials')
+  }
+})
+
+app.post('/register', async (req, res) => {
+  const { email, password } = req.body
+  const [rows] = await pool.promise()
+    .query('INSERT INTO `usuarios` (`email`, `password`) VALUES (?, ?)',
+      [email, password]);
+  if (rows.affectedRows > 0) {
+    res.send('Registration Successful')
+  } else {
+    res.send('Registration Failed')
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
